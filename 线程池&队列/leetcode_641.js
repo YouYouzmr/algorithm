590a842fa85847313963c9d0a4a8cda3695fbe2c
zhaimr <zhaimr@belink.com>
2021-07-05 17:50:55 +0800

    设计队列的实现方法

--- 线程池&队列/leetcode_641.js
+++ 线程池&队列/leetcode_641.js
@@ -1,27 +1,38 @@
 /**
  * 641. 设计循环双端队列
+ *  你的实现需要支持以下操作：
+ *
+ *  MyCircularDeque(k)：构造函数,双端队列的大小为k。
+ *  insertFront()：将一个元素添加到双端队列头部。 如果操作成功返回 true。
+ *  insertLast()：将一个元素添加到双端队列尾部。如果操作成功返回 true。
+ *  deleteFront()：从双端队列头部删除一个元素。 如果操作成功返回 true。
+ *  deleteLast()：从双端队列尾部删除一个元素。如果操作成功返回 true。
+ *  getFront()：从双端队列头部获得一个元素。如果双端队列为空，返回 -1。
+ *  getRear()：获得双端队列的最后一个元素。 如果双端队列为空，返回 -1。
+ *  isEmpty()：检查双端队列是否为空。
+ *  isFull()：检查双端队列是否满了。
  */
 /**
  * Initialize your data structure here. Set the size of the deque to be k.
  * @param {number} k
  */
- var MyCircularDeque = function(k) {
-    this.front = 0
-    this.rear = 0
-    // this.cont = 0
-    this.max = k
-    this.queue = Array(k+1)
+var MyCircularDeque = function(k) {
+    this.arr = Array(k)
+    this.head = 0;
+    this.tail = 0;
+    this.cnt = 0;
 };
 
 /**
- * Adds an item at the front of Deque. Return true if the operation is successful. 
+ * Adds an item at the front of Deque. Return true if the operation is successful.
  * @param {number} value
  * @return {boolean}
  */
 MyCircularDeque.prototype.insertFront = function(value) {
     if(this.isFull()) return false
-    this.front = (this.front + this.max) % (this.max +1)
-    this.queue[this.front] = value
+    this.arr[this.head] = value
+    this.head = (this.head + 1 + this.size()) % this.size()
+    this.cnt += 1
     return true
 };
 
@@ -32,8 +43,9 @@ MyCircularDeque.prototype.insertFront = function(value) {
  */
 MyCircularDeque.prototype.insertLast = function(value) {
     if(this.isFull()) return false
-    this.queue[this.rear] = value
-    this.rear = (this.rear+1)%(this.max +1)
+    this.tail = (this.tail-1+this.size()) % this.size()
+    this.arr[this.tail] = value
+    this.cnt += 1
     return true
 };
 
@@ -42,9 +54,10 @@ MyCircularDeque.prototype.insertLast = function(value) {
  * @return {boolean}
  */
 MyCircularDeque.prototype.deleteFront = function() {
-    if(this.isEmpty()) return false
-    this.front = (this.front+1)%(this.max+1)
-    return true
+    if(this.isEmpty()) return false;
+    this.head = (this.head-1+this.size()) % this.size()
+    this.cnt -= 1
+    return true;
 };
 
 /**
@@ -52,27 +65,27 @@ MyCircularDeque.prototype.deleteFront = function() {
  * @return {boolean}
  */
 MyCircularDeque.prototype.deleteLast = function() {
-    if(this.isEmpty()) return false
-    this.rear = (this.rear+ this.max) % (this.max + 1)
+    if(this.isEmpty()) return false;
+    this.tail = (this.tail+1+this.size())%this.size()
+    this.cnt -= 1
     return true
 };
-
 /**
  * Get the front item from the deque.
  * @return {number}
  */
 MyCircularDeque.prototype.getFront = function() {
-    if(this.isEmpty()) return -1
-    return this.queue[this.front]
+    if(this.isEmpty()) return -1;
+    return this.arr[(this.head-1+this.size())%this.size()]
 };
 
 /**
- * Get the last item from the deque.
+ * Get the last item from the deque
  * @return {number}
  */
 MyCircularDeque.prototype.getRear = function() {
-    if(this.isEmpty()) return -1
-    return this.queue[(this.rear+this.max)%(this.max+1)]
+    if(this.isEmpty()) return -1;
+    return this.arr[this.tail]
 };
 
 /**
@@ -80,7 +93,7 @@ MyCircularDeque.prototype.getRear = function() {
  * @return {boolean}
  */
 MyCircularDeque.prototype.isEmpty = function() {
-    return ((this.rear-this.front+this.max +1) %(this.max +1)) === 0
+    return this.cnt === 0
 };
 
 /**
@@ -88,8 +101,16 @@ MyCircularDeque.prototype.isEmpty = function() {
  * @return {boolean}
  */
 MyCircularDeque.prototype.isFull = function() {
-    return ((this.rear-this.front+ this.max+1)%(this.max + 1)) === this.max
+    return this.size()===this.cnt
 };
+
+/**
+ * get length of queue
+ * @return {number}
+ */
+MyCircularDeque.prototype.size = function() {
+    return this.arr.length
+}
 
 /**
  * Your MyCircularDeque object will be instantiated and called as such:
