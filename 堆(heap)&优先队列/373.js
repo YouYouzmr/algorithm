eb0c3d336072370dce35a92f084d27b7fb16232c
zhaimr <zhaimr@belink.com>
2021-07-22 18:06:42 +0800

    堆基础练习

---
+++ 堆(heap)&优先队列/373.js
@@ -0,0 +1,22 @@
+/**
+ * leetcode: 373. 查找和最小的K对数字
+ * 给定两个以升序排列的整数数组 nums1 和 nums2 , 以及一个整数 k 。
+ * 定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2 。
+ * 请找到和最小的 k 个数对 (u1,v1),  (u2,v2)  ...  (uk,vk) 
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kSmallestPairs = function(nums1, nums2, k) {
+    let result = []
+    return result
+};
+
+let nums1 = [1, 2, 3, 4];
+let nums2 = [2, 3, 4, 5, 6]
+let result = kSmallestPairs(nums1, nums2, 20)
+console.log(result)
