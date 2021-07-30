007465348ba53c9b7d329ed41c396fc7479441fe
zhaimr <zhaimr@belink.com>
2021-07-30 18:00:37 +0800

    leetcode: 10.正则表达式匹配，新增动态规划思路解析

--- leetcode/10.js
+++ leetcode/10.js
@@ -6,6 +6,10 @@
  *     '*' 匹配零个或多个前面的那一个元素
  * 
  * 所谓匹配，是要涵盖 整个 字符串s的，而不是部分字符串。
+
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/regular-expression-matching
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 /**
