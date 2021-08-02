49859c6cd0a96aa9264e2d80ac35b9d7690dd76a
YouYouzmr <595093508@qq.com>
2021-08-02 23:21:56 +0800

    leetcode: 295. 数据流的中位数

--- 堆(heap)&优先队列/view.17.20.js
+++ 堆(heap)&优先队列/view.17.20.js
@@ -1,17 +1,24 @@
 /**
- * leetcode: 面试题 17.20. 连续中值
+ * leetcode: 295. 数据流的中位数
  * 
- * 随机产生数字并传递给一个方法。
- * 你能否完成这个方法，在每次产生新值时，寻找当前所有值的中间值（中位数）并保存。
- * 
- * 中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值
+ * 中位数是有序列表中间的数。如果列表长度是偶数，
+ * 中位数则是中间两个数的平均值。
+ */
+
+/**
+ * 思路：
+ *  维护两个堆，左边大顶堆，右边小顶堆
+ *  如果为元素数量为奇数，则取大顶堆top
+ *  偶数取大小顶堆top 平均值
  */
 
 /**
  * initialize your data structure here.
  */
-var MedianFinder = function() {
-    this.heap = new Heap(1)
+ var MedianFinder = function() {
+    // 对顶堆
+    this.largeHeap = new Heap(1) // 前半段
+    this.smallHeap = new Heap(2) // 后半段
 };
 
 /** 
@@ -19,14 +26,31 @@ var MedianFinder = function() {
  * @return {void}
  */
 MedianFinder.prototype.addNum = function(num) {
+    if(this.largeHeap.size()==0 || num <= this.largeHeap.top()) {
+        this.largeHeap.push(num)
+    } else {
+        this.smallHeap.push(num)
+    }
+    if(this.smallHeap.size() > this.largeHeap.size()) {
+        this.largeHeap.push(this.smallHeap.pop())
+    }
+    if(this.largeHeap.size()===this.smallHeap.size() + 2) {
+        this.smallHeap.push(this.largeHeap.pop())
+    }
 
+    return
 };
 
 /**
  * @return {number}
  */
 MedianFinder.prototype.findMedian = function() {
+    let n = this.largeHeap.size() + this.smallHeap.size()
+    if(n%2==1) {
+        return this.largeHeap.top()
+    }
 
+    return (this.largeHeap.top()+this.smallHeap.top())/2
 };
 
 /**
