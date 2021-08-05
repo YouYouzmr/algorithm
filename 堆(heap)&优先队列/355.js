65082b86283d21f3ea8c95d334ec4e7b29b11d99
YouYouzmr <2394712858@qq.com>
2021-08-05 10:56:51 +0800

    leetcode: 355. 设计推特

--- 堆(heap)&优先队列/355.js
+++ 堆(heap)&优先队列/355.js
@@ -1,11 +1,14 @@
 /**
  * leetcode: 355. 设计推特
- * 设计一个简化版的推特(Twitter)，可以让用户实现发送推文，关注/取消关注其他用户，能够看见关注人（包括自己）的最近十条推文。你的设计需要支持以下的几个功能：
  * 
- * postTweet(userId, tweetId): 创建一条新的推文
- * getNewsFeed(userId): 检索最近的十条推文。每个推文都必须是由此用户关注的人或者是用户自己发出的。推文必须按照时间顺序由最近的开始排序。
- * follow(followerId, followeeId): 关注一个用户
- * unfollow(followerId, followeeId): 取消关注一个用户
+ * 设计一个简化版的推特(Twitter)，可以让用户实现发送推文，
+ * 关注/取消关注其他用户，能够看见关注人（包括自己）的最近十条推文。
+ * 
+ * 你的设计需要支持以下的几个功能：
+ *   postTweet(userId, tweetId): 创建一条新的推文
+ *   getNewsFeed(userId): 检索最近的十条推文。每个推文都必须是由此用户关注的人或者是用户自己发出的。推文必须按照时间顺序由最近的开始排序。
+ *   follow(followerId, followeeId): 关注一个用户
+ *   unfollow(followerId, followeeId): 取消关注一个用户
  *  
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/design-twitter
@@ -14,8 +17,11 @@
 /**
  * Initialize your data structure here.
  */
- var Twitter = function() {
 
+var stamp = 0
+var Twitter = function() {
+    // 存储用户信息
+    this.userMap = new Map()
 };
 
 /**
@@ -25,7 +31,12 @@
  * @return {void}
  */
 Twitter.prototype.postTweet = function(userId, tweetId) {
+    if(!this.userMap.has(userId)) {
+        this.userMap.set(userId, new User(userId))
+    }
 
+    let tweetsList = this.userMap.get(userId).tweets
+    tweetsList.unshift({id: tweetId, time: stamp++})
 };
 
 /**
@@ -34,7 +45,26 @@ Twitter.prototype.postTweet = function(userId, tweetId) {
  * @return {number[]}
  */
 Twitter.prototype.getNewsFeed = function(userId) {
+    let res = []
+    if(!this.userMap.has(userId)) return res
 
+    // 创建一个小顶堆记录, 记录
+    let heap = new Heap(CMP2)
+
+    // 获取关注人id 集合
+    let follows = [...this.userMap.get(userId).followList]
+
+    for(let i=0; i < follows.length; i++) {
+        if(!this.userMap.has(follows[i])) continue;
+        let tweets = this.userMap.get(follows[i]).tweets
+        for(let j=0; j<tweets.length; j++) {
+            if(heap.size()==10 && heap.top().time > tweets[j].time) break
+            heap.push(tweets[j])
+            if(heap.size()>10) heap.pop()
+        }
+    }
+    
+    return heap.output().sort((a, b)=> b.time-a.time).map(val=>val.id)
 };
 
 /**
@@ -44,7 +74,12 @@ Twitter.prototype.getNewsFeed = function(userId) {
  * @return {void}
  */
 Twitter.prototype.follow = function(followerId, followeeId) {
+    if(!this.userMap.has(followerId)) {
+        this.userMap.set(followerId, new User(followerId))
+    }
 
+    this.userMap.get(followerId).follow(followeeId)
+    return 
 };
 
 /**
@@ -54,9 +89,39 @@ Twitter.prototype.follow = function(followerId, followeeId) {
  * @return {void}
  */
 Twitter.prototype.unfollow = function(followerId, followeeId) {
-
+    if(this.userMap.has(followerId)) {
+        this.userMap.get(followerId).unfollow(followeeId)
+    }
+    return
 };
 
+// 创建用户实例
+var User = function(userId) {
+    this.id = userId
+    this.followList = new Set()
+    this.tweets = []
+
+    this.follow(userId)
+}
+
+// 用户关注其他用户
+User.prototype.follow = function(userId) {
+    this.followList.add(userId)
+}
+// 用户取消关注
+User.prototype.unfollow = function(userId) {
+    this.followList.delete(userId)
+}
+
+// 创建大顶堆
+function CMP1(a, b) {
+    return a.time < b.time
+}
+
+// 创建小顶堆
+function CMP2(a, b) {
+    return a.time > b.time
+}
 /**
  * Your Twitter object will be instantiated and called as such:
  * var obj = new Twitter()
@@ -65,3 +130,12 @@ Twitter.prototype.unfollow = function(followerId, followeeId) {
  * obj.follow(followerId,followeeId)
  * obj.unfollow(followerId,followeeId)
  */
+
+let twitter = new Twitter()
+twitter.postTweet(2, 5)
+twitter.getNewsFeed(2)
+twitter.follow(2, 3)
+twitter.postTweet(3, 6)
+twitter.getNewsFeed(2)
+twitter.unfollow(2, 3)
+twitter.getNewsFeed(2)
