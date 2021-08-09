169619fbfb72a7a189c6aea0d557b5fa560a6390
YouYouzmr <595093508@qq.com>
2021-08-09 22:29:30 +0800

    leetcode: 547. 省份数量

---
+++ 并查集(union-find)/547.js
@@ -0,0 +1,32 @@
+/**
+ * leetcode: 547. 省份数量
+ * 
+ * 有 n 个城市，其中一些彼此相连，另一些没有相连。
+ * 如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，
+ * 那么城市 a 与城市 c 间接相连。
+ * 
+ * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市
+ * 给你一个 n x n 的矩阵 isConnected ，
+ * 其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，
+ * 而 isConnected[i][j] = 0 表示二者不直接相连。
+ * 
+ * 返回矩阵中 省份 的数量。
+ */
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+    let n = isConnected.length
+    let union = new UnionSet(n)
+    for(let i=0; i<n; i++) {
+        for(let j=0; j<n; j++) {
+            isConnected[i][j] == 1 && union.merge(i+1, j+1)
+        }
+    }
+    let ans = 0
+    for(let i=1; i<=n; i++) {
+        if(union.get(i)===i) ans += 1
+    }
+    return ans
+};
