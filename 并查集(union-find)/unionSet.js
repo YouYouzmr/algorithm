c351be9eaed2514f88b5be2a9768d86144a2dcca
YouYouzmr <595093508@qq.com>
2021-08-09 22:02:39 +0800

    并查集模板

---
+++ 并查集(union-find)/unionSet.js
@@ -0,0 +1,18 @@
+// 并查集模板
+class unionSet {
+    constructor (n) {
+        this.boss = Array(n+1)
+        for(let i=0; i<=n; i++) {
+            this.boss[i] = i
+        }
+    }
+
+    get(x) {
+        return this.boss[x] = (this.boss[x]===x? x: this.get(this.boss[x]))
+    }
+
+    merge(a, b) {
+        this.boss[this.get(a)] = this.get(b)
+        return
+    }
+}
