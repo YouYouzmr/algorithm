a8ade982307a81dc4aba9ee551a37a79cb6385d1
YouYouzmr <2394712858@qq.com>
2021-08-11 17:31:18 +0800

    leetcode: 765. 情侣牵手

--- 并查集(union-find)/765.js
+++ 并查集(union-find)/765.js
@@ -47,18 +47,3 @@ var minSwapsCouples = function(row) {
 
     return ret
 };
-
-class UnionSet {
-    constructor (n) {
-        this.boss = Array(n+1).map((val, index)=> index)
-    }
-
-    get(x) {
-        return this.boss[x] = (this.boss[x]===x? x: this.get(this.boss[x]))
-    }
-
-    merge(a, b) {
-        this.boss[this.get(a)] = this.get(b)
-        return
-    }
-}
