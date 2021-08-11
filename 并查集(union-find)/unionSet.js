06febc9e00372517a6146806d6ef5a2d74fa4199
YouYouzmr <595093508@qq.com>
2021-08-11 23:57:21 +0800

    并查集类，计算每个集合总数

--- 并查集(union-find)/unionSet.js
+++ 并查集(union-find)/unionSet.js
@@ -13,3 +13,25 @@ class UnionSet {
         return
     }
 }
+
+// 记录每个集合的总数
+class UnionSetCnt {
+    constructor (n) {
+        this.boss = Array(n+1)
+        for(let i=0; i<=n; i++) {
+            this.boss[i] = i
+        }
+        this.cnt = Array(n+1).fill(1)
+    }
+
+    get(x) {
+        return this.boss[x] = (this.boss[x]===x? x: this.get(this.boss[x]))
+    }
+
+    merge(a, b) {
+        if(this.get(a)== this.get(b)) return
+        this.cnt[this.get(b)] += this.cnt[this.get(a)]
+        this.boss[this.get(a)] = this.get(b)
+        return
+    }
+}
