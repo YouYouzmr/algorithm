d11a0f9ec8cfec031c1d6365d673ce59cb3d4315
YouYouzmr <595093508@qq.com>
2021-08-26 23:46:35 +0800

    leetcode: 53. 最大子序和

--- leetcode/53.js
+++ leetcode/53.js
@@ -8,11 +8,12 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 最大前缀和
 var maxSubArray = function(nums) {
     let max = nums[0]
     let total
     for(let i=0; i< nums.length; i++) {
-        total = total>0? total +nums[i] : nums[i]
+        total = total>0? total + nums[i] : nums[i]
         max = Math.max(total, max)
     }
     return max
