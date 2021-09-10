90ebd6f3e1de44cbff84584a2088cda826001fb7
YouYouzmr <2394712858@qq.com>
2021-09-10 14:39:47 +0800

    leetcode: 491. 递增子序列(不下降子序列)

--- 算法系列(Algorithm算法杂谈)/491.js
+++ 算法系列(Algorithm算法杂谈)/491.js
@@ -7,29 +7,41 @@
  * 数组中可能含有重复元素，如出现两个整数相等，也可以视作递增序列的一种特殊情况。
  * 
  * 提示：
- * 1 <= nums.length <= 15
- * -100 <= nums[i] <= 100
+ *  1 <= nums.length <= 15
+ *  -100 <= nums[i] <= 100
  */
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-var getResult = function(nums, k, buff, ret) {
-    if(buff.length > 1) ret.push(buff);
-    buff.push(0);
-    // 去重
-    let can = []
-    for(let i = k; i < nums.length; i++) {
-        
-        if(buff.length == 1 || nums[i] >= buff[buff.length - 2]) {
-            buff[buff.length - 1] = nums[i]
-            getResult(nums, i + 1, buff, ret)
+var findSubsequences = function(nums) {
+    const ret = [];
+    const len = nums.length;
+    const set = new Set();
+
+    const dfs = (start, path) => {
+        if(path.length >= 2) {
+            const str = path.toString();
+            if(!set.has(str)) {
+                ret.push(path.slice());
+                set.add(str)
+            }
+        }
+
+        for(let i = start; i < len; i++) {
+            const prev = path[path.length - 1];
+            const cur = nums[i];
+            if(path.length == 0 || prev <= cur) {
+                path.push(cur);
+                dfs(i + 1, path);
+                path.pop()
+            }
         }
     }
-}
 
-var findSubsequences = function(nums) {
-    let ret = [];
-    getResult(nums, 0, [], ret);
+    dfs(0, [])
     return ret
 };
+
+let nums = [4, 6, 7, 7]
+findSubsequences(nums)
