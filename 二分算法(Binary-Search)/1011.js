af2a03cd4ea04722e3d6eb12ee9a6d3c56f74ec4
YouYouzmr <2394712858@qq.com>
2021-09-17 17:39:11 +0800

    leetcode: 1011. 在 D 天内送达包裹的能力

--- 二分算法(Binary-Search)/1011.js
+++ 二分算法(Binary-Search)/1011.js
@@ -13,6 +13,32 @@
  * @param {number} days
  * @return {number}
  */
- var shipWithinDays = function(weights, days) {
+var searchDay = function(nums, mid) {
+    // cnt: 总天数， cur: 一天运送包裹重量和
+    let cnt = 1, cur = 0;
+    for(let x of nums) {
+        if(cur + x > mid) {
+            cnt += 1;
+            cur = 0
+        } 
+        cur += x
+    }
+    return cnt;
+}
+var shipWithinDays = function(weights, days) {
+    // 首先统计货物总重量，并找出单个货物最大重量
+    // 这两个事船的载货数量左右峰值
+    let total = 0, weight = 0, mid;
+    for(let x of weights) {
+        total += x;
+        weight = Math.max(x, weight)
+    }
 
+    while(weight < total) {
+        mid = (weight + total) >> 1;
+        if(searchDay(weights, mid) <= days) total = mid
+        else weight = mid + 1
+    }
+
+    return total;
 };
