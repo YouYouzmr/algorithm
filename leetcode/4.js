739170072fefd5cc1a537c26f0468479ec726104
YouYouzmr <2394712858@qq.com>
2021-09-17 15:39:32 +0800

    leetcode: 4. 寻找两个正序数组的中位数 (二分、归并、其他)

--- leetcode/4.js
+++ leetcode/4.js
@@ -15,15 +15,19 @@
  * @param {number[]} nums2
  * @return {number}
  */
+
+let nums1 = [1, 3], nums2 = [2]
 // nums1[k/2] < nums2[k/2] 
 // nums1[k/2] 最多比 k-2个元素大，排名 k-1；
 // 二分算法： 二分的是问题规模
+
+// 找到处于k位置的值
 var findK = function(nums1, nums2, i, j, k) {
     if(i == nums1.length) return nums2[j + k - 1];
     if(j == nums2.length) return nums1[i + k - 1];
     if(k == 1) return nums1[i] < nums2[j]? nums1[i] : nums2[j];
-    let a = Math.min(k >> 1, nums1.length - 1);
-    let b = Math.min(k - a, nums2.lenght - 1);
+    let a = Math.min(k >> 1, nums1.length);
+    let b = Math.min(k - a, nums2.length);
     a = k - b;
     if(nums1[i + a - 1] <= nums2[j + b - 1]) {
         return findK(nums1, nums2, i + a, j, k - a);
@@ -40,31 +44,41 @@ var findMedianSortedArrays = function(nums1, nums2) {
     return (a + b) >> 1;
 }
 
+console.log(findMedianSortedArrays(nums1, nums2))
 
 
-// 归并排序
+
+// 第二种：归并排序
 // 时间复杂度: O(k)
 var findMedianSortedArrays = function(nums1, nums2) {
     let i = 0, j = 0;
     let m = nums1.length, n = nums2.length;
-    let mid = (m + n) >> 1;
-    let res = []
-    while(i < m && j < n && mid) {
+    let mid = (m + n + 1) >> 1;
+    let res = 0;
+    while(i < m || j < n) {
         if((i < m && nums1[i] < nums2[j]) || j >= n) {
+            res = nums1[i]
             i++; 
-            res.push(nums1[i])
         }
         else {
+            res = nums2[j]
             j++;
-            res.push(nums2[j])
         };
+        if(!--mid) {
+            if((m + n) % 2 === 0) {
+                let next1 = i < m ? nums1[i]: Infinity;
+                let next2 = j < n ? nums2[j]: Infinity;
+                res = (res + Math.min(next1, next2)) / 2;
+            }
+            break;
+        }
     }
     
-    // 
+    return res;
 }
 
 
-//
+// 第三种
 var findMedianSortedArrays = function(nums1, nums2) {
     const newArr = nums1.concat(nums2).sort((a, b) => a - b)
     
